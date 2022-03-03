91857818967a96a0c6b8bbb7fbf94b6236dc05ff
Your Name <you@example.com>
2022-03-03 17:54:21 +0700

    Update Responsive mobile

--- asset/js/index.js
+++ asset/js/index.js
@@ -1,3 +1,36 @@
+// RESPONSIVE 
+const header = document.getElementById('header')
+const mobileMenu = document.getElementById('mobile')
+const headerHight = header.clientHeight
+
+// Đóng mở menu mobile
+mobileMenu.onclick = function(){
+    let isClose = header.clientHeight === 46;
+    if(isClose){
+        header.style.height = 'auto'
+    }else{
+        header.style.height = null
+    }
+}
+
+
+//Close khi chọn menu
+const menuItems = document.querySelectorAll('#nav li a[href*="#"]')
+const sl = menuItems.length
+for(let i = 0; i < sl; i++){
+    let menuItem = menuItems[i]
+
+    menuItem.onclick = ()=>{
+        header.style.height = null
+    }
+}
+
+
+
+
+
+
+// BUY TICKETS MODAL
 const buyBtns = document.querySelectorAll('.js-buy-ticket')
 const modal = document.querySelector('.modal')
 const modalClose = document.querySelector('.js-modal-close')
