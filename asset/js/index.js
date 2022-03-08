881a64f94f7cb00a6cc62b4646dc557516f02e2c
Your Name <you@example.com>
2022-03-08 17:55:06 +0700

    update slider images

--- asset/js/index.js
+++ asset/js/index.js
@@ -2,6 +2,51 @@
 const header = document.getElementById('header')
 const mobileMenu = document.getElementById('mobile')
 const headerHight = header.clientHeight
+const slider = document.getElementById('slider')
+const h2Element = document.querySelector('.text-heading')
+const textDescription = document.querySelector('.text-description')
+
+
+//  Tạo Slider
+let sliderObject = [
+    {
+        id: 1,
+        img : "./asset/images/chicago.jpg",
+        address: "Chicago",
+        description: "Thank you, Chicago - A night we won't forget."
+    },
+    {
+        id: 2,
+        img : "./asset/images/la.jpg",
+        address: "Los Angeles",
+        description: "We had the best time playing at Venice Beach!"
+    },
+    {
+        id: 3,
+        img : "./asset/images/ny.jpg",
+        address: "New York",
+        description:"The atmosphere in New York is lorem ipsum."
+    }
+];
+
+let i = 0
+let slideItem = sliderObject.length
+const time = 3000
+function changeSlider(){
+    slider.style.background = `url('${sliderObject[i].img}') top center/ cover no-repeat`
+    h2Element.innerHTML = sliderObject[i].address
+    textDescription.innerHTML = sliderObject[i].description
+    
+    if(i < slideItem - 1){
+        i++;
+    }
+    else{
+        i = 0
+    }
+    setTimeout("changeSlider()",time)
+}
+
+window.onload = changeSlider
 
 // Đóng mở menu mobile
 mobileMenu.onclick = function(){
